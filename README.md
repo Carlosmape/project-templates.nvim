21c0762af4b18d91e8fa28fcd8b5e4aa38d053fd
= <=>
2022-05-19 13:07:58 +0200

    Added support to replace tokens in file names as well

--- README.md
+++ README.md
@@ -1,45 +1,41 @@
 # project-templates.nvim
 
-I made this plugin mainly out of pure frustration. I had been using the extension [Project Templates](https://marketplace.visualstudio.com/items?itemName=cantonios.project-templates) back when I used VSCode as my IDE, and I had come to rely on it. When I migrated to Neovim, I thought "this extension was sooo helpfull! Clearly there _must_ be an equivialent for vim, right?" I went searching and searching, but I couldn't find _anything_. There were plugins that created templates for individual files, and even multiple related files, like [templator_vim](https://github.com/tomtom/templator_vim), but nothing that creates templates for an entire project, that defines the project structure and boilerplate code for each necessary file. In most IDEs, the IDE will take care of these things for you automatically when you start a new project. But with vim, you have to create each individual file and folder yourself. This plugin aims to fix that.
+This plugin allows to manage projects templates using easily from nvim command-line.
+
+With some extra features:
+- File names can contain tokens as well  
 
 ## Demo
 ![](demo.gif)
 
-# Quick Start
-
-## Requirements
-
-You will need neovim compiled with python3 support. To determine whether or not your neovim has python support, type `:echo has('python3')` and press enter. If it outputs `1`, you have python3 support, otherwise you will need to run the command `pip install pynvim`.
-
-You will also need to run the command `pip install binaryornot`.
-
-
-## Installation
-
-Add this to your init.vim:
-
-- vim-plug:
-  - `Plug 'pianocomposer321/project-templates.nvim', { 'do': ':UpdateRemotePlugins' }`
- 
-- Vundle:
-  - `Plugin 'pianocomposer321/project-templates.nvim', { 'do':  ':UpdateRemotePlugins'}`
-
-etc, etc.
-
-Then run `:PlugInstall` (vim-plug) or `:PluginInstall` (Vundle).
-
-Finally, restart Neovim.
-
-# Usage
-
+# Usage  
 - `:LoadTemplate` - Load a template into a new project. The plugin will look for placeholders (e.g. `#{PLACEHOLDER}`), and ask for values to replace them in each non-binary file
 - `:DeleteTemplate` - Delete a template
 - `:SaveAsTemplate` - Save the current folder and all files and subfolders as a new template
 
-The plugin will look for templates in ~/.templates. Sample templates can be found at https://github.com/pianocomposer321/project-template-samples. If you want to create your own templates, simply create a new project, add the placeholders you want, and save it in ~/.templates, or use the `:SaveAsTemplate` command.
+The plugin will look for templates in `~/.templates`. Sample templates can be found at [Carlosmape/project-template-samples](https://github.com/Carlosmape/project-template-samples). If you want to create your own templates, simply create a new project, add the placeholders you want, and save it manually in `~/.templates`, or use the `:SaveAsTemplate` command.
+
+# Quick Start
+## Requirements
+You will need neovim compiled with python3 support. To determine whether or not your neovim has python support, type `:echo has('python3')` and press enter. If it outputs `1`, you have python3 support, otherwise you will need to run the command `pip install pynvim`.
+
+You will also need to run the command `pip install binaryornot`.
+
+## Installation
+Add this to your init.vim:
+
+- Packer:
+  - `use 'Carlosmape/project-templates.nvim'`
+
+- vim-plug:
+  - `Plug 'Carlosmape/project-templates.nvim', { 'do': ':UpdateRemotePlugins' }`
+ 
+- Vundle:
+  - `Plugin 'Carlosmape/project-templates.nvim', { 'do':  ':UpdateRemotePlugins'}`
+
+Then run `:PackerInstall` (Packer), `:PlugInstall` (vim-plug) or `:PluginInstall` (Vundle) or whatever plugin manager you use command to install new plugins.
+
+Finally, restart Neovim.
 
 # Credits
-
-Finder.vim - https://vim.fandom.com/wiki/Implement_your_own_interactive_finder_without_plugins
-
-Inspired by [this](https://github.com/bit101/ProjectMaker) Sublime text plugin and [this](https://github.com/cantonios/vscode-project-templates) VS Code Extension.
+Original repository  [pianocomposer321/project-templates.nvim](https://github.com/pianocomposer321/project-templates.nvim) 
--- rplugin/python3/ProjectTemplates.py
+++ rplugin/python3/ProjectTemplates.py
@@ -12,6 +12,7 @@ class ProjectTemplate(object):
         self.projectDir = f"{os.path.expanduser('~')}/.templates"
         self.projects = {}
         self.tokenized_files = []
+        self.tokenized_file_names = []
         self.tokens = []
         self.token_values = []
 
@@ -45,55 +46,61 @@ class ProjectTemplate(object):
         self.vim.out_write(f"\nTemplate {to_load} Loaded Successfully.\n")
 
     def getTokensFromProject(self, projectName):
-        # for currentfolder, subfolders, files in os.walk(os.getcwd()):
+        r = re.compile(r'#{[^}]+}')
         for currentfolder, subfolders, files in os.walk(projectName):
             for file in files:
-                if not is_binary(os.path.join(currentfolder, file)):
-                    with open(os.path.join(currentfolder, file), 'r') as tosearch:
+                file_path = os.path.join(currentfolder, file)
+                if not is_binary(file_path):
 
+                    # Look for tokenized file names (tokens are in the file name)
+                    file_matches = r.findall(file)
+                    for file_match in file_matches:
+                        print('{file_match} token in file {file_path}')
+                        if file_path not in self.tokenized_file_names:
+                            self.tokenized_file_names.append(file_path)
+                        if file_match not in self.tokens:
+                            self.tokens.append(file_match)
+
+                    # Look for tokenized files (tokens are inside file contents)
+                    with open(file_path, 'r') as tosearch:
                         contents = tosearch.read()
-                        r = re.compile(r'#{[^}]+}')
                         matches = r.findall(contents)
-
-                        print(f'{matches} in file {os.path.join(currentfolder, file)}')
+                        print(f'{matches} in file {file_path}')
 
                         for match in matches:
                             if match not in self.tokens:
                                 self.tokens.append(match)
 
-                    if os.path.join(currentfolder, file) not in self.tokenized_files:
-                        self.tokenized_files.append(os.path.join(currentfolder, file))
+                    if file_path not in self.tokenized_files:
+                        self.tokenized_files.append(file_path)
 
         print(self.tokens)
 
     def replaceTokens(self):
-        # for file, token in self.tokens:
-        #     with open(file, 'r') as toread:
-        #         r = re.compile(token)
-        #         content = toread.read()
-        #         print(r.findall(content))
-        #     with open(file, 'w') as towrite:
-        #         token_value = self.vim.eval(f'input("Enter the value for the token {token}> ")')
-        #         content = r.sub(token_value, content)
-        #         towrite.write(content)
-
+        """User insert new loaded tokens values and does replacement where it is needed""" 
+        # Ask user to replace token with desired value
         for token in self.tokens:
             token_value = self.vim.eval(f'input("Enter the value for the token {token}> ")')
             self.token_values.append((token, token_value))
-
+        
+        # Replace tokens in file's content with given value
         for file in self.tokenized_files:
             with open(file, 'r') as toread:
                 content = toread.read()
-            # with open(file, 'w') as towrite:
-            #     for token, value in self.token_values:
-            #         r = re.compile(token)
-            #         content = r.sub(value, content)
-            #         towrite.write(content)
             for token, value in self.token_values:
                 r = re.compile(token)
                 content = r.sub(value, content)
             with open(file, 'w') as towrite:
                 towrite.write(content)
+
+        # Replace tokens in file's names with given value
+        for file in self.tokenized_file_names:
+            for token, value in self.token_values:
+                # Ensure this token is inside this tokenized file name
+                if token in file: 
+                    r = re.compile(token)
+                    file_name = r.sub(value, file)
+                    os.rename(file, file_name)
 
     @pynvim.command("SaveAsTemplate", sync=True)
     def saveAsTemplate(self):
